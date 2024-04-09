c2dab68eb0e2085de0187a9a51d1d9cc0065ba9b
studbaneso1322 <soumyadip.banerjee@stud.th-rosenheim.de>
2024-04-09 20:03:07 +0200

    deploy

--- footer/urls.py
+++ footer/urls.py
@@ -5,8 +5,11 @@ app_name = "community"
 
 urlpatterns = [
     path('contact/', views.contact, name='contact'),
+    path('contribute/', views.contribute, name='contribute'),
+    path('sponsor/', views.sponsor, name='sponsor'),
     path('guidelines/', views.guidelines, name='guidelines'),  
     path('about/', views.about, name='about'),  
     path('facts/',views.facts,name='facts')
+
 ]
 
