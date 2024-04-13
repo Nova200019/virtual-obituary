0f5ca25d4180f368676c0016b5fbe1b7e71da19b
Nova200019 <76976441+Nova200019@users.noreply.github.com>
2024-04-13 23:05:31 +0200

    Update settings.py

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -88,7 +88,7 @@ DATABASES = {
         'PORT': '3306',  # Default MySQL port
         'OPTIONS': {
             'ssl': {
-                 'ca': os.path.join(BASE_DIR, 'CA.crt.pem'),
+                 'ca': os.path.join(BASE_DIR, 'DigiCertGlobalRootCA.crt.pem'),
             },
             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",  # Optional: Enforces strict SQL mode
         },
